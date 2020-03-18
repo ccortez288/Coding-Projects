e729f1787e29602ca684242f3375bcd29720aee3
ccortez288 <52089007+ccortez288@users.noreply.github.com>
2020-03-18 01:11:07 -0700

    Create dbx.h

---
+++ C++ Projects/HexDump/dbx.h
@@ -0,0 +1,9 @@
+/* Author: Christian Cortez
+ * Class Account: cssc1965
+ * CS530, Spring 2020
+ * Assignment #1, HexDump/BinDUmp
+ * Filename: dbx.h
+ */
+
+void binaryDump(unsigned char *, long);
+void hexDump(unsigned char *, long);
